f32923109f5bf35eb0a43b238f940b1f179758b2
Gabsca2001 <scamardo.website@gmail.com>
2025-03-09 14:20:47 +0100

    env file creation

--- washingMachine/src/firebase-services/firebase-config.mjs
+++ washingMachine/src/firebase-services/firebase-config.mjs
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBe-Xj364uZQyUPXu4hoCKiRu8BxpphfFE",
-    authDomain: "booking-laundry-liborio.firebaseapp.com",
-    projectId: "booking-laundry-liborio",
-    storageBucket: "booking-laundry-liborio.firebasestorage.app",
-    messagingSenderId: "505464444736",
-    appId: "1:505464444736:web:80a50556fcd24428544ca5"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
